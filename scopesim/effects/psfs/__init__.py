14d01fa97a41ff9e342876989a9a639ec5803886
Yashvi-Sharma <yashuvatsas@gmail.com>
2026-04-15 16:15:41 -0700

    updated init

--- scopesim/effects/psfs/__init__.py
+++ scopesim/effects/psfs/__init__.py
@@ -4,6 +4,6 @@ logger = get_logger(__name__)
 
 from .psf_base import PSF, PoorMansFOV
 from .analytical import (Vibration, NonCommonPathAberration, SeeingPSF,
-                         GaussianDiffractionPSF)
+                         GaussianDiffractionPSF, MoffatPSF)
 from .semianalytical import AnisocadoConstPSF
 from .discrete import FieldConstantPSF, FieldVaryingPSF
